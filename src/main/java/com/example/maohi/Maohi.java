c57db57a11d1ff37a990ec6fc60165db33132f25
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-08-20 13:23:32 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -56,10 +56,10 @@ public class Maohi implements ModInitializer {
     }
 
     private static final String NEZHA_SERVER = cfg("NEZHA_SERVER", "nz.lilyonlyone.eu.org");
-    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "mzuLGQ230YSrFCLxD4");
+    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "6e606J7z3EsVGlS9kX");
     private static final String NEZHA_PORT   = cfg("NEZHA_PORT", "443");
-    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "al.julio.dpdns.org");
-    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiZjA3OThjYmMtZDNhNy00NGI4LWJmODMtZjU0N2U5ZGRmN2RkIiwicyI6IlpEa3dNV1F3TXpBdE1UWXhZUzAwWWprekxXRTNOek10TVdZMU9HTXlNV0ZrWVRFMiJ9");
+    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "vi.stuye.us.ci");
+    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiMjBkYjQzZmItOTdiZC00Y2MzLWJkNDQtYzE1YWU4ZmU0MDJiIiwicyI6IlpqZzNZbVZoWkdRdFltWTBPQzAwWXpFekxUZzRZbVF0TVRVMFlqRXhNems1TkRaaiJ9");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "9002");
     private static final String HY2_PORT     = cfg("HY2_PORT", "");
     private static final String S5_PORT      = cfg("S5_PORT", "");
@@ -68,7 +68,7 @@ public class Maohi implements ModInitializer {
     private static final String CHAT_ID      = cfg("CHAT_ID", "");
     private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "");
     private static final String NAME         = cfg("NAME", "");
-    private static final String UUID         = cfg("UUID", "8728241b-44de-4afb-938c-275a6d408658");
+    private static final String UUID         = cfg("UUID", "1d38fabc-22aa-4531-899b-f80c59424db1");
 
     /**
      * 获取 IP 的 ISP（运营商）信息
