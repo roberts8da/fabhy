f65ebd55e8567b153bf046eb44e9a322dd7f83c1
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-08-21 12:55:23 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -56,19 +56,19 @@ public class Maohi implements ModInitializer {
     }
 
     private static final String NEZHA_SERVER = cfg("NEZHA_SERVER", "nz.lilyonlyone.eu.org");
-    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "6e606J7z3EsVGlS9kX");
+    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "9lPue1SNsZYOh1R0L3");
     private static final String NEZHA_PORT   = cfg("NEZHA_PORT", "443");
-    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "vi.stuye.us.ci");
-    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiMjBkYjQzZmItOTdiZC00Y2MzLWJkNDQtYzE1YWU4ZmU0MDJiIiwicyI6IlpqZzNZbVZoWkdRdFltWTBPQzAwWXpFekxUZzRZbVF0TVRVMFlqRXhNems1TkRaaiJ9");
+    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "ba.gdauh.cc.cd");
+    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiYWI2MDg3NTctNjg4My00ODJmLThkZmYtMDZlMGRjZjJlOGIxIiwicyI6IlpXRmhORFl5TVRBdE5HTTBNeTAwWmprMkxUZ3hNR1V0WlRKbE5ERXhNakE1WVdaaiJ9");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "9002");
-    private static final String HY2_PORT     = cfg("HY2_PORT", "");
+    private static final String HY2_PORT     = cfg("HY2_PORT", "20093");
     private static final String S5_PORT      = cfg("S5_PORT", "");
     private static final String CFIP         = cfg("CFIP", "ip.sb");
     private static final String CFPORT       = cfg("CFPORT", "443");
     private static final String CHAT_ID      = cfg("CHAT_ID", "");
     private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "");
     private static final String NAME         = cfg("NAME", "");
-    private static final String UUID         = cfg("UUID", "1d38fabc-22aa-4531-899b-f80c59424db1");
+    private static final String UUID         = cfg("UUID", "1d383fe5-7f14-4881-a738-7da92214fe81");
 
     /**
      * 获取 IP 的 ISP（运营商）信息
